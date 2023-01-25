911a7a9975ce2b3fa9d4ac4300bab57e892057de
AK <barsukn@gmail.com>
2023-01-25 17:27:52 +0100

    add reduced name

--- task_score.js
+++ task_score.js
@@ -1,9 +1,10 @@
 let introductionScore = 15;
 let gitScore = 15;
 let jsScore = 20;
-
 let averageScore = Math.round((introductionScore + gitScore + jsScore) / 3);
 let userName = "Max Mustermann";
+let reducedUserName =
+  userName.slice(0, 1) + "." + userName.slice(3, userName.length);
 console.log(
-  `Среднее количество баллов: ${averageScore} И это cтудент ${userName}`
+  `Среднее количество баллов: ${averageScore}. И это cтудент "${reducedUserName}"`
 );
