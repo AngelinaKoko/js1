8e8cc5b45e50aa0f80ebf9a478bf8407b776c044
AK <barsukn@gmail.com>
2023-01-25 16:16:47 +0100

    add task_score.js

---
+++ task_score.js
@@ -0,0 +1,9 @@
+let introductionScore = 15;
+let gitScore = 15;
+let jsScore = 20;
+
+let averageScore = Math.round((introductionScore + gitScore + jsScore) / 3);
+let userName = "Max Mustermann";
+console.log(
+  `Среднее количество баллов: ${averageScore} И это cтудент ${userName}`
+);
